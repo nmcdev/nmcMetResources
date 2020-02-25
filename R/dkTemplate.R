3d06ba5f6fd215ed2f552007ef316731b9640a43
NMC-DAVE <daikan1998@163.com>
2020-02-25 22:18:47 +0800

    Add nmc notebook and html template.

--- R/dkTemplate.R
+++ R/dkTemplate.R
@@ -1,4 +1,4 @@
-# Features of the NMC clean markdown Template
+# Features of the NMC html markdown Template
 #' NMC Clean Markdown Template
 #' Refer to https://github.com/holtzy/epuRate
 #'
@@ -9,22 +9,54 @@
 #' @return
 #' @export
 #'
-#' @examples
-nmcCleanNB <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+nmcHtmlTemp <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
   
   # get the locations of resource files located within the package
-  css <- system.file("rmarkdown", "templates", "nmc_nb_clean_template" ,"resources", "style.css", package = "nmcMetResources")
-  template <- system.file("rmarkdown", "templates", "nmc_nb_clean_template" ,"resources", "template_epurate.html", package = "nmcMetResources")
+  css <- system.file("rmarkdown", "templates", "nmc_html_template" ,"resources", "style.css", package = "nmcMetResources")
+  template <- system.file("rmarkdown", "templates", "nmc_html_template" ,"resources", "template_nmc.html", package = "nmcMetResources")
   
   # call the base html_notebook function
-  rmarkdown::html_notebook( theme= "lumen",
+  rmarkdown::html_document( theme= "lumen",
                             template= template,
                             css= css,
                             toc= toc,
                             toc_float = TRUE,
-                            toc_depth = 2,
+                            toc_depth = 3,
+                            mathjax = "default",
                             number_sections= number_sections,
                             df_print = "paged",
                             code_folding = code_folding,
+  )
+}
+
+
+# Features of the NMC notebook markdown Template
+#' NMC Clean Markdown Template
+#' Refer to https://github.com/holtzy/epuRate
+#'
+#' @param toc 
+#' @param code_folding 
+#' @param number_sections 
+#'
+#' @return
+#' @export
+#'
+nmcNotebookTemp <- function(toc = TRUE, code_folding = "show", number_sections=TRUE) {
+  
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "nmc_notebook_template" ,"resources", "corp-styles.css", package = "nmcMetResources")
+  
+  # call the base html_notebook function
+  rmarkdown::html_notebook( theme= "default",
+                            css= css,
+                            toc= toc,
+                            toc_float = TRUE,
+                            toc_depth = 3,
+                            number_sections= number_sections,
+                            code_folding = code_folding,
+                            highlight = "pygments",
+                            fig_width = 10,
+                            fig_height = 8,
+                            self_contained = TRUE,
   )
 }
